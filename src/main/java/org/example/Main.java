0f62b7d09aa5fac9372970473385cd12183405b9
karol <karolufn@gmail.com>
2024-05-21 22:21:47 +0200

    First commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+// Stwórz serwis ktory bd umozliwial przyjmowanie, zapisywanie, wyswietlanie, edytowanie i usuwanie samochodow
+// CZytanie danych z konsoli albo JSON
+// Niech kazdy samochod ma ID, marke, model, kolor, rok produkcji
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
