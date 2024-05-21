e7afad6c47e37760e3f092e7ca98bd6aac918abd
karol <karolufn@gmail.com>
2024-05-21 22:34:58 +0200

    created car class

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -2,4 +2,37 @@ package org.example;
 
 public class Car {
 
+    private final String id;
+    private final String brand;
+    private final String model;
+    private final String color;
+    private final int year;
+
+    Car(String id, String brand, String model, String color, int year) {
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.year = year;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getYear() {
+        return year;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 // Stwórz serwis ktory bd umozliwial przyjmowanie, zapisywanie, wyswietlanie, edytowanie i usuwanie samochodow
 // CZytanie danych z konsoli albo JSON
+// Potrzebujemy serwis z repozytorium w ktorym bedziemy przechowywac samochody
 // Niech kazdy samochod ma ID, marke, model, kolor, rok produkcji
 
 public class Main {
